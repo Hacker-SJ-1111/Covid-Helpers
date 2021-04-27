27cbdfb9902f2bb522d8c2c50d980df355dfce97
sanat <sanatjha4@gmail.com>
2021-04-27 09:30:42 +0530

    Main

--- covidhelpers/settings.py
+++ covidhelpers/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'covidhelpers.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'static/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("",views.home,name="Home"),
     path("addhelp",views.addhelp,name="Add Help"),
     path("newhelp",views.newhelp,name="New Help"),
+    path("test",views.test,name="test"),
 ]
