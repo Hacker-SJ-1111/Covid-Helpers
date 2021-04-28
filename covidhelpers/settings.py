e0025a9aa96170be462a718d608e47970e943ece
sanat <sanatjha4@gmail.com>
2021-04-28 10:02:46 +0530

    Main

--- covidhelpers/settings.py
+++ covidhelpers/settings.py
@@ -82,26 +82,28 @@ WSGI_APPLICATION = 'covidhelpers.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
+# DATABASES = {
 
-    'default': {
+#     'default': {
 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'test',
+#         'NAME': 'test',
 
-        'USER': 'postgres',
+#         'USER': 'postgres',
 
-        'PASSWORD': 'sanatdanger',
+#         'PASSWORD': 'sanatdanger',
 
-        'HOST': 'localhost',
+#         'HOST': 'localhost',
 
-        'PORT': '5432',
+#         'PORT': '5432',
 
-    }
+#     }
 
-}
-
+# }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASEs['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
