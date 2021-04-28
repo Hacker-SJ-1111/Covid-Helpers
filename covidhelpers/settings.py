00138d8a3f434334cd4a884083219ca46c634a2d
sanat <sanatjha4@gmail.com>
2021-04-28 09:50:41 +0530

    Main

--- covidhelpers/settings.py
+++ covidhelpers/settings.py
@@ -76,13 +76,31 @@ WSGI_APPLICATION = 'covidhelpers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'test',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'sanatdanger',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
